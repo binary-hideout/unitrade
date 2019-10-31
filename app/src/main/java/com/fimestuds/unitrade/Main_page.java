da86fc897268c2224fffcdfa342702650ecf5558
Diegobr21 <jdiegobrana@gmail.com>
2019-10-31 17:55:38 -0600

    main page layout changes

--- app/src/main/java/com/fimestuds/unitrade/Main_page.java
+++ app/src/main/java/com/fimestuds/unitrade/Main_page.java
@@ -6,15 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Main_page extends AppCompatActivity {
 
-    public ImageButton back, perfil;
+    public ImageView perfil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-        back=findViewById(R.id.back_login);
         perfil=findViewById(R.id.btn_perfil);
 
         perfil.setOnClickListener(new View.OnClickListener() {
@@ -24,19 +24,11 @@ public class Main_page extends AppCompatActivity {
             }
         });
 
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                back2login();
-            }
-        });
+
     }
 
 
-    public void back2login(){
-        Intent intent1 = new Intent(this, Login.class );
-        startActivity(intent1);
-    }
+
     public void verperfil(){
         Intent intentper = new Intent(this, Perfil.class );
         startActivity(intentper);
