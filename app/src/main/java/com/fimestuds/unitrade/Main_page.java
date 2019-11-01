cf8d066064b3d8d003da5905cdf4df91e0b42a16
Diegobr21 <jdiegobrana@gmail.com>
2019-10-31 18:19:09 -0600

    Imagen class created

--- app/src/main/java/com/fimestuds/unitrade/Main_page.java
+++ app/src/main/java/com/fimestuds/unitrade/Main_page.java
@@ -1,21 +1,34 @@
 package com.fimestuds.unitrade;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterViewFlipper;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
-public class Main_page extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
+public class Main_page extends AppCompatActivity {
+    private AdapterViewFlipper flip_inicio;
+    private List<Articulo> arrayarticulos;
+    FloatingActionButton vender;
+    RecyclerView recyclerView;
+    Context context;
     public ImageView perfil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
         perfil=findViewById(R.id.btn_perfil);
+        vender=findViewById(R.id.floatingvender);
 
         perfil.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +37,12 @@ public class Main_page extends AppCompatActivity {
             }
         });
 
+        /*recyclerView = (RecyclerView) findViewById(R.id.recyclerView_inicio);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(layoutManager);
+
+*/
 
     }
 
